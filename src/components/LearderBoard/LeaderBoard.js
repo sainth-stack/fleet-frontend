b3d751db64780a1c79b55e8a4831dfced51a7367
VishnuVardhanBonala-web4U <rvardhancm08@gmail.com>
2024-11-22 23:57:06 +0530

    "updated dashboard"

---
+++ src/components/LearderBoard/LeaderBoard.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Leaderboard = ({ data }) => {
+  const sortedData = [...data].sort(
+    (a, b) => a.fuelConsumption - b.fuelConsumption
+  );
+
+    const top5Data = sortedData.slice(0, 5);
+  return (
+    <div className="p-3 max-w-xl mb-3  mx-auto bg-gray-50 rounded-lg shadow-lg">
+      <h2 className="text-center text-lg font-semibold text-gray-800 mb-6">
+        Fuel Consumption Leaderboard
+      </h2>
+      <div className="space-y-2 ">
+        {top5Data.length > 0 ? (
+          top5Data.map((trip, index) => (
+            <div
+              key={trip.id}
+              className="flex   p-3 bg-white rounded-lg shadow-md"
+            >
+              <div className="text-xl mx-3  font-bold text-blue-600">
+                {index + 1}
+              </div>
+              <div className="text-lg  text-gray-700 flex-1">{trip.driver}</div>
+              <div className="text-lg  text-green-600 text-right">
+                {trip.fuelConsumption} L
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Leaderboard;
--- src/components/RightSidebar/RightSidebar.js
+++ src/components/RightSidebar/RightSidebar.js
@@ -3,21 +3,15 @@ import Card from '../Card/Card';
 import DoughnutChartPage from "../../pages/Charts/DoughnutChartPage";
 import MultiBarChartPage from '../../pages/Charts/MultiBarChartPage';
 import VerticalMultiGraph from '../../pages/Charts/VerticalMultiGraphPage';
+import { tableData } from '../../data/TableData';
+import Leaderboard from '../LearderBoard/LeaderBoard';
 
 const RightSidebar = () => {
   return (
     <>
-      <div className="shadow-md  mr-3 mt-3  " style={{minHeight:"100%"}}>
+      <div className="shadow-md  mr-3 mt-3  " style={{ minHeight: "100%" }}>
         <div className="">
-          <Card
-            bgColor="bg-yellow-50"
-            styles="text-center"
-            title="Total Tips"
-            subtitle="Best"
-            price={99.99}
-            hit="Limited Offer!"
-            image="https://via.placeholder.com/150"
-          />
+          <Leaderboard data={tableData} />
         </div>
         <div className="min-h-40 overflow-auto">
           {/* <MultiBarChartPage /> */}
